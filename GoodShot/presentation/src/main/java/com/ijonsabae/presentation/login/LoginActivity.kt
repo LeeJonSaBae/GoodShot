0f7bd3caddd7ddf05446dd077ea5bf607a20cad2
HanguelMansae <111sym@naver.com>
2024-09-03 19:40:09 +0900

    🐛fix : ViewBinding 에러 수정 NameConvention을 착각해서 반대로 했던 것 수정..

--- GoodShot/presentation/src/main/java/com/ijonsabae/presentation/login/LoginActivity.kt
+++ GoodShot/presentation/src/main/java/com/ijonsabae/presentation/login/LoginActivity.kt
@@ -3,10 +3,11 @@ package com.ijonsabae.presentation.login
 import android.os.Bundle
 import com.ijonsabae.presentation.R
 import com.ijonsabae.presentation.config.BaseActivity
+import com.ijonsabae.presentation.databinding.ActivityLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LoginActivity: BaseActivity<LoginActivityBinding>(LoginActivity Binding::inflate) {
+class LoginActivity: BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
