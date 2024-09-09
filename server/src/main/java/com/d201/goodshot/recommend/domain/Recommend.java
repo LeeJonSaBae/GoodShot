0618c1cd75c371f5d5d0ca3c62c05168e49b2505
김민경 <org9899@naver.com>
2024-09-09 15:14:02 +0900

    ➕feat : 영상추천 Domain 생성

--- server/src/main/java/com/d201/goodshot/recommend/domain/Recommend.java
+++ server/src/main/java/com/d201/goodshot/recommend/domain/Recommend.java
@@ -1,9 +1,6 @@
 package com.d201.goodshot.recommend.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Recommend {
@@ -11,4 +8,8 @@ public class Recommend {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String videoUrl;
+    
 }
