6ace0e6bcdd8fda74bd6d637b32cd3403f174728
김민경 <org9899@naver.com>
2024-09-09 15:09:48 +0900

    ➕feat : 포즈 Domain 생성

--- server/src/main/java/com/d201/goodshot/swing/domain/Pose.java
+++ server/src/main/java/com/d201/goodshot/swing/domain/Pose.java
@@ -1,9 +1,6 @@
 package com.d201.goodshot.swing.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Pose {
@@ -11,5 +8,14 @@ public class Pose {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private int code; // 포즈 번호
+
+    private String feedback;
+
+    @Lob
+    private byte[] swingImage;
+
+    private double similarity; 
 
 }
