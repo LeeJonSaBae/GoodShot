1c341ec6e2b07580a7881b86fc3d1737a8524ace
김민경 <org9899@naver.com>
2024-09-09 15:12:53 +0900

    ➕feat : 전문가 Domain 생성

--- server/src/main/java/com/d201/goodshot/expert/domain/Expert.java
+++ server/src/main/java/com/d201/goodshot/expert/domain/Expert.java
@@ -1,4 +1,25 @@
 package com.d201.goodshot.expert.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Expert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int expYears;
+
+    private String fields;
+
+    private String certificate;
+
+    private String topic;
+
 }
