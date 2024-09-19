d36a0dba7e243ad0d589c4b4d03ecd2ecd00d135
김민경 <org9899@naver.com>
2024-09-19 17:01:55 +0900

    ➕feat : controller swagger Tag 적용

--- server/src/main/java/com/d201/goodshot/recommendation/controller/RecommendationController.java
+++ server/src/main/java/com/d201/goodshot/recommendation/controller/RecommendationController.java
@@ -1,9 +1,11 @@
 package com.d201.goodshot.recommendation.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/recommendation")
+@Tag(name = "recommendation")
 public class RecommendationController {
 }
--- server/src/main/java/com/d201/goodshot/user/controller/UserController.java
+++ server/src/main/java/com/d201/goodshot/user/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.d201.goodshot.user.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "user")
 public class UserController {
 
 
