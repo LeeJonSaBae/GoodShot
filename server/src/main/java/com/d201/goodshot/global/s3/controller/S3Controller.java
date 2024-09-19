3f60016d7f5b50b072c8cc4f2d2306d7865b1123
김민경 <org9899@naver.com>
2024-09-19 13:36:06 +0900

    ➕feat : Presinged 발급 API 생성

--- server/src/main/java/com/d201/goodshot/global/s3/controller/S3Controller.java
+++ server/src/main/java/com/d201/goodshot/global/s3/controller/S3Controller.java
@@ -1,7 +1,11 @@
 package com.d201.goodshot.global.s3.controller;
 
+import com.d201.goodshot.global.base.ApiResponse;
+import com.d201.goodshot.global.s3.dto.ImageRequest.PresignedUrlRequest;
+import com.d201.goodshot.global.s3.dto.ImageResponse.PresignedUrlResponse;
 import com.d201.goodshot.global.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +16,9 @@ public class S3Controller {
 
     private final S3Service s3Service;
 
-
+    public ApiResponse<PresignedUrlResponse> uploadImage(@RequestBody PresignedUrlRequest presignedUrlReq) {
+        PresignedUrlResponse response = s3Service.issuePresignedUrl(presignedUrlReq);
+        return ApiResponse.created(response);
+    }
 
 }
