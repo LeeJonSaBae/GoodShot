e86c15093b9ebe9f3227d5cb195539081aceb3f8
minseon <alstjs1516@gmail.com>
2024-09-19 18:01:02 +0900

    ➕feat - 139 : Home UI 구현 중

--- android/presentation/src/main/java/com/ijonsabae/presentation/home/HomeFragment.kt
+++ android/presentation/src/main/java/com/ijonsabae/presentation/home/HomeFragment.kt
@@ -1,13 +1,83 @@
 package com.ijonsabae.presentation.home
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.ijonsabae.presentation.R
 import com.ijonsabae.presentation.config.BaseFragment
 import com.ijonsabae.presentation.databinding.FragmentHomeBinding
+import com.ijonsabae.presentation.main.MainActivity
+import com.ijonsabae.presentation.replay.HorizontalMarginItemDecoration
+import kotlin.math.abs
 
-class HomeFragment: BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home) {
+class HomeFragment :
+    BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home) {
+
+    private var newsList = mutableListOf<News>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        (fragmentContext as MainActivity).hideAppBar()
+
+        initAppBarMotionLayout()
+        initNewsViewPager(binding.vpNews)
+
     }
+
+    private fun initAppBarMotionLayout() {
+        val motionLayout = binding.motionLayout
+        binding.appbar.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+            motionLayout.progress = seekPosition
+        }
+    }
+
+    private fun initNewsViewPager(viewPager: ViewPager2) {
+        val adapter = NewsViewPagerAdapter(requireContext()).apply { submitList(newsList) }
+        viewPager.adapter = adapter
+
+        viewPager.offscreenPageLimit = 1
+        viewPager.overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+        val itemDecoration = HorizontalMarginItemDecoration(horizontalMarginInDp = 20)
+        viewPager.addItemDecoration(itemDecoration)
+        viewPager.setPageTransformer { page, position ->
+            val offsetX = position * -(2 * 120) // offset 값으로 간격 조정
+            page.translationX = offsetX
+
+            val scale = 1 - abs(position) // scale 값으로 양쪽 애들 높이 조정
+            page.scaleY = 0.85f + 0.15f * scale
+        }
+        autoScroll(viewPager, 5000)
+    }
+
+    private fun autoScroll(viewPager: ViewPager2, delay: Long) {
+        val handler = Handler(Looper.getMainLooper())
+        val runnable = object : Runnable {
+            override fun run() {
+                viewPager.currentItem += 1
+                handler.postDelayed(this, delay) // 스크롤 간격
+            }
+        }
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable, delay)
+            }
+        })
+        handler.post(runnable)
+    }
+
+    private fun setNewsData() {
+//        return listOf(
+//            News(
+//                title = "훅 vS 슬라이스", description = "다르지만 비슷한 스윙 교정 방법에\n대해 알아볼까요?!"
+//            )
+//        )
+    }
+
 }
---
+++ android/presentation/src/main/java/com/ijonsabae/presentation/home/NewsViewPagerAdapter.kt
@@ -0,0 +1,52 @@
+package com.ijonsabae.presentation.home
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ijonsabae.presentation.R
+import com.ijonsabae.presentation.databinding.ItemNewsBinding
+
+class NewsViewPagerAdapter(val context: Context) :
+    ListAdapter<News, NewsViewPagerAdapter.NewsViewHolder>(Comparator) {
+
+    companion object Comparator : DiffUtil.ItemCallback<News>() {
+        override fun areItemsTheSame(oldItem: News, newItem: News): Boolean {
+            return System.identityHashCode(oldItem) == System.identityHashCode(newItem)
+        }
+
+        override fun areContentsTheSame(oldItem: News, newItem: News): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    inner class NewsViewHolder(private val binding: ItemNewsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            val item = getItem(position)
+            binding.tvNewsTitle.text = item.title
+            binding.tvNewsDescription.text = item.description
+            Glide.with(binding.root).load(context.resources.getDrawable(R.drawable.golf_dummy_img))
+                .into(binding.ivNewsImg)
+
+            binding.root.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(item.link))
+                binding.root.context.startActivity(intent)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val binding = ItemNewsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NewsViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        holder.bind(position)
+    }
+}
