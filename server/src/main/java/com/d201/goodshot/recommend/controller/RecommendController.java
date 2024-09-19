f065c08c7d4147ca441cd5a82822fc1978e33fb3
김민경 <org9899@naver.com>
2024-09-19 13:37:52 +0900

    ➕feat : recommend domain 삭제

--- server/src/main/java/com/d201/goodshot/recommend/controller/RecommendController.java
+++
@@ -1,9 +0,0 @@
-package com.d201.goodshot.recommend.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/recommendation")
-public class RecommendController {
-}
--- server/src/main/java/com/d201/goodshot/recommend/service/RecommendService.java
+++
@@ -1,9 +0,0 @@
-package com.d201.goodshot.recommend.service;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional
-public class RecommendService {
-}
