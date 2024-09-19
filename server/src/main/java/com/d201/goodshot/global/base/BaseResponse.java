7e802526c1e8c70cfaa505e6c820e4875ae8a9a3
김민경 <org9899@naver.com>
2024-09-19 17:34:19 +0900

    🔧chore : 공통 API Response 파일명 변경

--- server/src/main/java/com/d201/goodshot/global/base/BaseResponse.java
+++ server/src/main/java/com/d201/goodshot/global/base/BaseResponse.java
@@ -6,29 +6,29 @@ import org.springframework.http.HttpStatus;
 import static org.springframework.http.HttpStatus.*;
 
 @Getter
-public class ApiResponse<T> {
+public class BaseResponse<T> {
 
     private final int code;
     private final HttpStatus status;
     private final String message;
     private final T data;
 
-    private ApiResponse(HttpStatus status, String message, T data) {
+    private BaseResponse(HttpStatus status, String message, T data) {
         this.code = status.value();
         this.status = status;
         this.message = message;
         this.data = data;
     }
 
-    public static <T> ApiResponse<T> of(HttpStatus status, String message, T data) {
-        return new ApiResponse<>(status, message, data);
+    public static <T> BaseResponse<T> of(HttpStatus status, String message, T data) {
+        return new BaseResponse<>(status, message, data);
     }
 
-    public static <T> ApiResponse<T> ok(T data) {
+    public static <T> BaseResponse<T> ok(T data) {
         return of(OK, "SUCCESS", data);
     }
 
-    public static <T> ApiResponse<T> created(T data) {
+    public static <T> BaseResponse<T> created(T data) {
         return of(CREATED, "CREATED", data);
     }
 
