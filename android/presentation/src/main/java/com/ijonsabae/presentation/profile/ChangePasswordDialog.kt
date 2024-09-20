f6cf17d782226cdee84afc1c39b78d007fc56495
minseon <alstjs1516@gmail.com>
2024-09-20 17:54:35 +0900

    ➕feat - 136 : Profile 비밀번호 변경 다이얼로그 만드는 중

---
+++ android/presentation/src/main/java/com/ijonsabae/presentation/profile/ChangePasswordDialog.kt
@@ -0,0 +1,83 @@
+package com.ijonsabae.presentation.profile
+
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import com.ijonsabae.presentation.databinding.DialogChangePasswordBinding
+
+class ChangePasswordDialog : DialogFragment() {
+    private var _binding: DialogChangePasswordBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogChangePasswordBinding.inflate(inflater, container, false)
+        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ivChangePasswordClose.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val displayMetrics = DisplayMetrics()
+        requireActivity().windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val screenWidth = getScreenWidth(this.requireContext())
+//        val screenHeight = getScreenHeight(this.requireContext())
+
+        val newWidth = (screenWidth * 0.85).toInt()
+        val layoutParams = requireView().layoutParams
+        layoutParams.width = newWidth
+        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
+        requireView().layoutParams = layoutParams
+    }
+
+    private fun getScreenWidth(context: Context): Int {
+        val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics = wm.currentWindowMetrics
+            val insets = windowMetrics.windowInsets
+                .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+            windowMetrics.bounds.width() - insets.left - insets.right
+        } else {
+            val displayMetrics = DisplayMetrics()
+            wm.defaultDisplay.getMetrics(displayMetrics)
+            displayMetrics.widthPixels
+        }
+    }
+
+    private fun getScreenHeight(context: Context): Int {
+        val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics = wm.currentWindowMetrics
+            val insets = windowMetrics.windowInsets
+                .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+            windowMetrics.bounds.height() - insets.bottom - insets.top
+        } else {
+            val displayMetrics = DisplayMetrics()
+            wm.defaultDisplay.getMetrics(displayMetrics)
+            displayMetrics.heightPixels
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- android/presentation/src/main/java/com/ijonsabae/presentation/profile/ProfileFragment.kt
+++ android/presentation/src/main/java/com/ijonsabae/presentation/profile/ProfileFragment.kt
@@ -7,11 +7,21 @@ import com.ijonsabae.presentation.config.BaseFragment
 import com.ijonsabae.presentation.databinding.FragmentProfileBinding
 import com.ijonsabae.presentation.main.MainActivity
 
-class ProfileFragment: BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::bind, R.layout.fragment_profile) {
+private const val TAG = "굿샷_ProfileFragment"
+
+class ProfileFragment :
+    BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::bind, R.layout.fragment_profile) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (fragmentContext as MainActivity).showAppBar("마이 페이지")
 
+        binding.layoutChangePassword.setOnClickListener {
+            showCustomDialog()
+        }
+    }
 
+    private fun showCustomDialog() {
+        val customDialog = ChangePasswordDialog()
+        customDialog.show(parentFragmentManager, "CustomDialogFragment")
     }
 }
