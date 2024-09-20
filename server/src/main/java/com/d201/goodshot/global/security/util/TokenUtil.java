5fa72db8ce499b8f447b8c6cf98404c17acaa57c
김민경 <org9899@naver.com>
2024-09-20 17:27:16 +0900

    ➕feat - 194 : TokenUtil 생성

--- server/src/main/java/com/d201/goodshot/global/security/util/TokenUtil.java
+++ server/src/main/java/com/d201/goodshot/global/security/util/TokenUtil.java
@@ -1,2 +1,114 @@
-package com.d201.goodshot.global.security.util;public class TokenUtil {
+package com.d201.goodshot.global.security.util;
+
+import com.d201.goodshot.global.security.dto.CustomUser;
+import com.d201.goodshot.global.security.dto.Token;
+import com.d201.goodshot.user.domain.User;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.d201.goodshot.global.security.exception.SecurityExceptionList.*;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class TokenUtil {
+
+    private final Long accessTokenExpireTime = 60 * 60L; // 1시간
+    private SecretKey secretKey;
+
+    // secretKey 초기화
+    public void generateSecretKey(@Value("${security.secret-key}") String secret) {
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    // 토큰 검증
+    public String getSubject(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getSubject();
+    }
+
+    public Object getClaim(String token, String claimName, Class<?> classType) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get(claimName, classType);
+    }
+
+    // 토큰 생성
+    public Token generateToken(User user) {
+        String accessToken = Jwts.builder()
+                .subject(user.getEmail())
+                .issuedAt(Timestamp.valueOf(LocalDateTime.now()))
+                .claim("email", user.getEmail())
+                .expiration(Date
+                        .from(Instant.now()
+                        .plus(accessTokenExpireTime, ChronoUnit.SECONDS)))
+                .signWith(secretKey).compact();
+        return Token.builder().accessToken(accessToken).build();
+    }
+
+    public Authentication getAuthentication(CustomUser user) {
+        List<GrantedAuthority> grantedAuthorities = user.getRoles().stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        return new UsernamePasswordAuthenticationToken(user, "",
+                grantedAuthorities);
+    }
+
+    public void makeAuthentication(User user) {
+        // Authentication 정보 만들기
+        CustomUser customUser = CustomUser.builder()
+                .email(user.getEmail())
+                .roles(Arrays.asList(user.getRole().toString()))
+                .build();
+
+        // ContextHolder 에 Authentication 정보 저장
+        Authentication auth = getAuthentication(customUser);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    public boolean validateToken(String token, HttpServletRequest request) {
+        try {
+            Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload();
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+            request.setAttribute("exception", MALFORMED_TOKEN.getErrorCode());
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+            request.setAttribute("exception", EXPIRED_TOKEN.getErrorCode());
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+            request.setAttribute("exception", UNSUPPORTED_TOKEN.getErrorCode());
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+            request.setAttribute("exception", ILLEGAL_TOKEN.getErrorCode());
+        } catch (Exception e) {
+            log.info(e.getMessage());
+            request.setAttribute("exception", ACCESS_DENIED.getErrorCode());
+        }
+        return false;
+    }
+
 }
