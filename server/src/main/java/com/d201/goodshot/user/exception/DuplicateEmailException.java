4e99679f64c07197e29e3c0f08ee0b5702467964
김민경 <org9899@naver.com>
2024-09-20 13:04:00 +0900

    ➕feat - 191 : 회원가입 이메일 중복 확인 예외 처리

--- server/src/main/java/com/d201/goodshot/user/exception/DuplicateEmailException.java
+++ server/src/main/java/com/d201/goodshot/user/exception/DuplicateEmailException.java
@@ -1,2 +1,9 @@
-package com.d201.goodshot.user.exception;public class DupulicateEmailException {
+package com.d201.goodshot.user.exception;
+
+import static com.d201.goodshot.user.exception.UserExceptionList.DUPLICATE_USER_EMAIL;
+
+public class DuplicateEmailException extends UserException{
+    public DuplicateEmailException() {
+        super(DUPLICATE_USER_EMAIL.getErrorCode(), DUPLICATE_USER_EMAIL.getHttpStatus(), DUPLICATE_USER_EMAIL.getMessage());
+    }
 }
--- server/src/main/java/com/d201/goodshot/user/exception/UserExceptionList.java
+++ server/src/main/java/com/d201/goodshot/user/exception/UserExceptionList.java
@@ -1,4 +1,17 @@
 package com.d201.goodshot.user.exception;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserExceptionList {
+
+    DUPLICATE_USER_EMAIL("U001", HttpStatus.CONFLICT, "이미 가입된 이메일입니다.");
+
+    private final String errorCode;
+    private final HttpStatus httpStatus;
+    private final String message;
+
 }
