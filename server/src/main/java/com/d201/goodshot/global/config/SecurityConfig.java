628c74298bce110b007d005677365156a3bd7771
김민경 <org9899@naver.com>
2024-09-20 13:03:20 +0900

    ➕feat -191 : SecurityConfig 생성 (스프링 시큐리티 세팅)

---
+++ server/src/main/java/com/d201/goodshot/global/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.d201.goodshot.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsUtils;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(auth -> auth.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((a) -> {
+                    a.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                            .requestMatchers("/member/signup/**").permitAll()
+                            .anyRequest().authenticated();
+                });
+
+        return http.build();
+    }
+
+}
---
+++ server/src/main/java/com/d201/goodshot/user/dto/UserResponse.java
@@ -0,0 +1,2 @@
+package com.d201.goodshot.user.dto;public class UserResponse {
+}
