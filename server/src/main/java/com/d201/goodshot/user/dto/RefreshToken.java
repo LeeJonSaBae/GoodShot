d9744fb7406bcd0857285ff7d2181c04b3403530
김민경 <org9899@naver.com>
2024-09-23 19:59:12 +0900

    ➕feat - 191 : RefreshToken Dto, Repository 생성

--- server/src/main/java/com/d201/goodshot/user/dto/RefreshToken.java
+++ server/src/main/java/com/d201/goodshot/user/dto/RefreshToken.java
@@ -1,2 +1,19 @@
-package com.d201.goodshot.user.dto;public class RefreshToken {
+package com.d201.goodshot.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash(value = "RefreshToken", timeToLive = 604800) // 일주일
+public class RefreshToken {
+    @Id
+    private String email; // key
+    private String refreshToken; // value
 }
--- server/src/main/java/com/d201/goodshot/user/repository/RefreshTokenRepository.java
+++ server/src/main/java/com/d201/goodshot/user/repository/RefreshTokenRepository.java
@@ -1,2 +1,9 @@
-package com.d201.goodshot.user.repository;public interface RefreshTokenRepository {
+package com.d201.goodshot.user.repository;
+
+import com.d201.goodshot.user.dto.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 }
