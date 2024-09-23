2aa98e98e63594a9f4b3dffeabe60e0a72f3b8d6
김민경 <org9899@naver.com>
2024-09-23 16:37:17 +0900

    ➕feat - 191 : TokenResponse 생성

--- server/src/main/java/com/d201/goodshot/global/security/dto/TokenResponse.java
+++ server/src/main/java/com/d201/goodshot/global/security/dto/TokenResponse.java
@@ -1,2 +1,14 @@
-package com.d201.goodshot.global.security.dto;public class TokenResponse {
+package com.d201.goodshot.global.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+    private String accessToken;
 }
