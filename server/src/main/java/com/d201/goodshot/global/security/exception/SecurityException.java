86d7e479f2e6df0013a42be3ab94ce801a598cfa
김민경 <org9899@naver.com>
2024-09-23 22:13:30 +0900

    ➕feat - 196 : SecurityException 생성

--- server/src/main/java/com/d201/goodshot/global/security/exception/SecurityException.java
+++ server/src/main/java/com/d201/goodshot/global/security/exception/SecurityException.java
@@ -1,2 +1,10 @@
-package com.d201.goodshot.global.security.exception;public class SecurityException {
+package com.d201.goodshot.global.security.exception;
+
+import com.d201.goodshot.global.exception.ApplicationException;
+import org.springframework.http.HttpStatus;
+
+public class SecurityException extends ApplicationException {
+    protected SecurityException(String errorCode, HttpStatus httpStatus, String message) {
+        super(errorCode, httpStatus, message);
+    }
 }
