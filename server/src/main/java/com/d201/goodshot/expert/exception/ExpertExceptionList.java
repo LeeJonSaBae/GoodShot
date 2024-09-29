e33aca7cf6075e5d2acd269ebf77f06040bd3d96
김민경 <org9899@naver.com>
2024-09-30 00:36:59 +0900

    ➕feat - 201 : 전문가 예외 처리 추가

--- server/src/main/java/com/d201/goodshot/expert/exception/ExpertExceptionList.java
+++ server/src/main/java/com/d201/goodshot/expert/exception/ExpertExceptionList.java
@@ -1,4 +1,16 @@
 package com.d201.goodshot.expert.exception;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
 public enum ExpertExceptionList {
+
+    NOT_FOUND_EXPERT(HttpStatus.NOT_FOUND.value(), "존재하지 않는 전문가입니다.");
+
+    private final int code;
+    private final String message;
+
 }
