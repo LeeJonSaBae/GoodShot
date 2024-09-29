6fcc42942b825b6a7b50365f8707ac816d8d32aa
김민경 <org9899@naver.com>
2024-09-30 00:37:19 +0900

    ➕feat - 201 : 전문가 상세 목록 조회 API 생성

--- server/src/main/java/com/d201/goodshot/expert/service/ExpertService.java
+++ server/src/main/java/com/d201/goodshot/expert/service/ExpertService.java
@@ -1,16 +1,18 @@
 package com.d201.goodshot.expert.service;
 
 import com.d201.goodshot.expert.domain.Expert;
-import com.d201.goodshot.expert.dto.ExpertResponse;
+import com.d201.goodshot.expert.dto.ExpertResponse.ExpertDetailItem;
 import com.d201.goodshot.expert.dto.ExpertResponse.ExpertItem;
 import com.d201.goodshot.expert.dto.ExpertResponse.ExpertItemResponse;
 import com.d201.goodshot.expert.repository.ExpertRepository;
+import com.d201.goodshot.user.exception.NotFoundUserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,6 +44,20 @@ public class ExpertService {
                 .pageNo(pageNo)
                 .pageSize(pageSize)
                 .hasNext(expertSlice.hasNext())
+                .build();
+    }
+
+    public ExpertDetailItem getExpertDetail(int id) {
+        Expert expert = expertRepository.findById(id).orElseThrow(NotFoundUserException::new);
+
+        List<String> certificates = Arrays.asList(expert.getCertificate().split(", "));
+
+        return ExpertDetailItem
+                .builder()
+                .imageUrl(expert.getProfileUrl())
+                .name(expert.getName())
+                .certificates(certificates)
+                .expYears(expert.getExpYears())
                 .build();
     }
 
