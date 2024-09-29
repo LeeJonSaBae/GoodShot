0cc544d6158576e8074484fce5f8cb2cbcd82a8c
김민경 <org9899@naver.com>
2024-09-30 00:51:39 +0900

    ➕feat : 내 프로필 조회 API 생성

--- server/src/main/java/com/d201/goodshot/user/dto/UserResponse.java
+++ server/src/main/java/com/d201/goodshot/user/dto/UserResponse.java
@@ -1,5 +1,19 @@
 package com.d201.goodshot.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class UserResponse {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ProfileResponse{
+        String profileUrl;
+        String name;
+    }
 
 }
