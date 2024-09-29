a3353fb0a75182ae485ec019d01a3fb2269157d0
김민경 <org9899@naver.com>
2024-09-30 00:09:26 +0900

    ➕feat - 282 : 전문가 목록 조회 API 생성

--- server/src/main/java/com/d201/goodshot/expert/service/ExpertService.java
+++ server/src/main/java/com/d201/goodshot/expert/service/ExpertService.java
@@ -1,9 +1,18 @@
 package com.d201.goodshot.expert.service;
 
+import com.d201.goodshot.expert.domain.Expert;
+import com.d201.goodshot.expert.dto.ExpertResponse;
+import com.d201.goodshot.expert.dto.ExpertResponse.ExpertItem;
+import com.d201.goodshot.expert.dto.ExpertResponse.ExpertItemResponse;
 import com.d201.goodshot.expert.repository.ExpertRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -12,6 +21,28 @@ public class ExpertService {
 
     private final ExpertRepository expertRepository;
 
+    public ExpertItemResponse getExpertList(int pageNo, int pageSize) {
+        PageRequest pageRequest = PageRequest.of(pageNo, pageSize);
+        Slice<Expert> expertSlice = expertRepository.findAllBy(pageRequest);
+        List<Expert> experts = expertSlice.getContent();
 
+        List<ExpertItem> expertItems = experts
+                .stream().map(expert -> ExpertItem.
+                            builder()
+                            .imageUrl(expert.getProfileUrl())
+                            .name(expert.getName())
+                            .field(expert.getField())
+                            .expYears(expert.getExpYears())
+                            .build())
+                .collect(Collectors.toList());
+
+        return ExpertItemResponse.
+                builder()
+                .experts(expertItems)
+                .pageNo(pageNo)
+                .pageSize(pageSize)
+                .hasNext(expertSlice.hasNext())
+                .build();
+    }
 
 }
