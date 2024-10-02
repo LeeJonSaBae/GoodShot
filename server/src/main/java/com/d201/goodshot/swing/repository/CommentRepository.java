083bcba975f8d381160122179a25514cea476a85
김민경 <org9899@naver.com>
2024-10-02 15:02:28 +0900

    ➕feat : Comment, SwingImage, Swing Repository 추가

--- server/src/main/java/com/d201/goodshot/swing/repository/CommentRepository.java
+++ server/src/main/java/com/d201/goodshot/swing/repository/CommentRepository.java
@@ -1,2 +1,10 @@
-package com.d201.goodshot.swing.repository;public interface CommentRepository {
+package com.d201.goodshot.swing.repository;
+
+import com.d201.goodshot.swing.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
 }
--- server/src/main/java/com/d201/goodshot/swing/repository/SwingImageRepository.java
+++ server/src/main/java/com/d201/goodshot/swing/repository/SwingImageRepository.java
@@ -1,2 +1,8 @@
-package com.d201.goodshot.swing.repository;public interface SwingImageRepository {
+package com.d201.goodshot.swing.repository;
+
+import com.d201.goodshot.swing.domain.SwingImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SwingImageRepository extends JpaRepository<SwingImage, Integer> {
+
 }
