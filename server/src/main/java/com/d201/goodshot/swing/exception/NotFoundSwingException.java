c1633c24ed67ce8ed84f7682aacf09e20363e747
김민경 <org9899@naver.com>
2024-10-02 15:02:09 +0900

    ➕feat : SwingException 추가 (존재하는지)

--- server/src/main/java/com/d201/goodshot/swing/exception/NotFoundSwingException.java
+++ server/src/main/java/com/d201/goodshot/swing/exception/NotFoundSwingException.java
@@ -1,2 +1,9 @@
-package com.d201.goodshot.swing.exception;public class NotFoundSwingException {
+package com.d201.goodshot.swing.exception;
+
+import static com.d201.goodshot.swing.exception.SwingExceptionList.NOT_FOUND_SWING;
+
+public class NotFoundSwingException extends SwingException{
+    public NotFoundSwingException() {
+        super(NOT_FOUND_SWING.getCode(), NOT_FOUND_SWING.getMessage());
+    }
 }
--- server/src/main/java/com/d201/goodshot/swing/exception/SwingException.java
+++ server/src/main/java/com/d201/goodshot/swing/exception/SwingException.java
@@ -1,2 +1,9 @@
-package com.d201.goodshot.swing.exception;public class SwingException {
+package com.d201.goodshot.swing.exception;
+
+import com.d201.goodshot.global.exception.ApplicationException;
+
+public class SwingException extends ApplicationException {
+    protected SwingException(int code, String message) {
+        super(code, message);
+    }
 }
