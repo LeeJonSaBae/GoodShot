db3d7a19fdbc98bb1fea946a15eec4e452315007
김민경 <org9899@naver.com>
2024-10-02 15:00:57 +0900

    🐛fix : swing entity 수정 swingImage 연관관계 수정

--- server/src/main/java/com/d201/goodshot/swing/domain/Swing.java
+++ server/src/main/java/com/d201/goodshot/swing/domain/Swing.java
@@ -1,12 +1,19 @@
 package com.d201.goodshot.swing.domain;
 
+import com.d201.goodshot.swing.dto.SwingData;
 import com.d201.goodshot.user.domain.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -38,5 +45,19 @@ public class Swing {
     private int score;
 
     private double tempo;
+
+    private String title;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime time;
+
+    @OneToMany(mappedBy = "swing", cascade = CascadeType.ALL)
+    private List<SwingImage> swingImages = new ArrayList<>();
+
+    public void update(SwingData swingData) {
+
+    }
+
 
 }
