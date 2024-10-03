7b0e50354a1b4f344517697e551778c90fc52ab4
김민경 <org9899@naver.com>
2024-10-03 15:03:12 +0900

    🐛fix : SwingData, Swing, Comment, SwingRequest Dto 수정

--- server/src/main/java/com/d201/goodshot/swing/domain/Swing.java
+++ server/src/main/java/com/d201/goodshot/swing/domain/Swing.java
@@ -1,6 +1,7 @@
 package com.d201.goodshot.swing.domain;
 
 import com.d201.goodshot.swing.dto.SwingData;
+import com.d201.goodshot.swing.exception.SwingVideoProcessingException;
 import com.d201.goodshot.user.domain.User;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -52,12 +54,26 @@ public class Swing {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime time;
 
-    @OneToMany(mappedBy = "swing", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "swingImage", cascade = CascadeType.ALL)
     private List<SwingImage> swingImages = new ArrayList<>();
 
-    public void update(SwingData swingData) {
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
 
+    public void updateSwing(SwingData swingData) {
+        this.solution = swingData.getSolution();
+        this.score = swingData.getScore();
+        this.tempo = swingData.getTempo();
+        this.likeStatus = swingData.isLikeStatus();
+        this.title = swingData.getTitle();
+        this.time = swingData.getTime();  // 날짜도 갱신
+        this.similarity = swingData.getPoseSimilarity().toString(); // JSON 필드도 갱신
+
+        try {
+            this.swingVideo = swingData.getSwingVideo().getBytes();
+        } catch (IOException e) {
+            throw new SwingVideoProcessingException();
+        }
     }
-
 
 }
