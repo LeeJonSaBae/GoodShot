82a44bb7a733f65e7554d7a8c4d4d93e6c14527c
HanguelMansae <111sym@naver.com>
2024-10-03 16:19:23 +0900

    🐛fix : Expert Detail DTO, Dialog Nav arg 및 로직 수정 dialog 생성 전에 Detail 정보를 불러서 건네주는 것으로 Dialog가 빈 화면으로 잠깐 있던 현상 개선

--- android/presentation/src/main/java/com/ijonsabae/presentation/consult/ConsultViewModel.kt
+++ android/presentation/src/main/java/com/ijonsabae/presentation/consult/ConsultViewModel.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.ijonsabae.domain.model.CommonResponse
 import com.ijonsabae.domain.model.Expert
+import com.ijonsabae.domain.model.ExpertDetail
+import com.ijonsabae.domain.usecase.consult.GetConsultantInfoUseCase
 import com.ijonsabae.domain.usecase.consult.GetConsultantListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
@@ -13,6 +16,11 @@ import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
-class ConsultViewModel @Inject constructor(private val getConsultantListUseCase: GetConsultantListUseCase) : ViewModel() {
+class ConsultViewModel @Inject constructor(
+    private val getConsultantListUseCase: GetConsultantListUseCase,
+    private val getConsultantInfoUseCase: GetConsultantInfoUseCase) : ViewModel() {
     val consultantList: Flow<PagingData<Expert>> = getConsultantListUseCase().cachedIn(viewModelScope).distinctUntilChanged()
+    suspend fun getConsultantInfo(id: Int): Result<CommonResponse<ExpertDetail>>{
+            return getConsultantInfoUseCase(id)
+    }
 }
