992b50dc410ca64267bf5545e6fdc39193efdf4e
권오윤 <rdd2033@naver.com>
2024-10-06 03:04:17 +0900

    🐛fix : shotOptionDialog에 클럽이 아이언으로 기본 선택되어 있도록 변경

--- android/presentation/src/main/java/com/ijonsabae/presentation/shot/ShotSettingViewModel.kt
+++ android/presentation/src/main/java/com/ijonsabae/presentation/shot/ShotSettingViewModel.kt
@@ -1,26 +1,39 @@
 package com.ijonsabae.presentation.shot
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ShotSettingViewModel @Inject constructor(): ViewModel() {
+class ShotSettingViewModel @Inject constructor() : ViewModel() {
     private val _isLeft: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val isLeft: StateFlow<Boolean>
         get() = _isLeft
 
-    suspend fun setIsLeftStatus(isLeft: Boolean){
+    private val _selectedGolfClub = MutableStateFlow("아이언")
+    val selectedGolfClub: StateFlow<String>
+        get() = _selectedGolfClub.asStateFlow()
+
+    suspend fun setIsLeftStatus(isLeft: Boolean) {
         _isLeft.emit(isLeft)
+    }
+
+    fun setSelectedGolfClub(club: String) {
+        viewModelScope.launch {
+            _selectedGolfClub.emit(club)
+        }
     }
 
     private val _showMidReport: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val showMidReport: StateFlow<Boolean>
         get() = _showMidReport
 
-    suspend fun setShowMidReportStatus(status: Boolean){
+    suspend fun setShowMidReportStatus(status: Boolean) {
         _showMidReport.emit(status)
     }
 
@@ -28,7 +41,7 @@ class ShotSettingViewModel @Inject constructor(): ViewModel() {
     val totalSwingCnt: StateFlow<Int>
         get() = _totalSwingCnt
 
-    suspend fun setTotalSwingCnt(cnt: Int){
+    suspend fun setTotalSwingCnt(cnt: Int) {
         _totalSwingCnt.emit(cnt)
     }
 
