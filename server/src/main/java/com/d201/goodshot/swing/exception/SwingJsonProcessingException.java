0f65e052320a4e0875e9e325b1e5fbce845470a2
김민경 <org9899@naver.com>
2024-10-06 22:35:33 +0900

    ➕feat : JSON 파싱 Exception 추가

--- server/src/main/java/com/d201/goodshot/swing/exception/SwingJsonProcessingException.java
+++ server/src/main/java/com/d201/goodshot/swing/exception/SwingJsonProcessingException.java
@@ -1,9 +1,9 @@
 package com.d201.goodshot.swing.exception;
 
-import static com.d201.goodshot.swing.exception.SwingExceptionList.SWING_VIDEO_PROCESS_ERROR;
+import static com.d201.goodshot.swing.exception.SwingExceptionList.SWING_JSON_PROCESS_ERROR;
 
-public class SwingVideoProcessingException extends SwingException{
-    public SwingVideoProcessingException() {
-        super(SWING_VIDEO_PROCESS_ERROR.getCode(), SWING_VIDEO_PROCESS_ERROR.getMessage());
+public class SwingJsonProcessingException extends SwingException{
+    public SwingJsonProcessingException() {
+        super(SWING_JSON_PROCESS_ERROR.getCode(), SWING_JSON_PROCESS_ERROR.getMessage());
     }
 }
