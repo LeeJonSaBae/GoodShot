34e153cf04f548582dac94384e266abfd8978656
김민경 <org9899@naver.com>
2024-10-08 02:19:54 +0900

    ➕feat : problemType에 대한 enum 못 찾았을 때 Exception

--- server/src/main/java/com/d201/goodshot/swing/exception/NotFoundProblemTypeException.java
+++ server/src/main/java/com/d201/goodshot/swing/exception/NotFoundProblemTypeException.java
@@ -1,4 +1,10 @@
 package com.d201.goodshot.swing.exception;
 
-public class NotFoundProblemType {
+
+import static com.d201.goodshot.swing.exception.SwingExceptionList.NOT_FOUND_REPORT_COMMENT;
+
+public class NotFoundProblemTypeException extends SwingException {
+    public NotFoundProblemTypeException() {
+        super(NOT_FOUND_REPORT_COMMENT.getCode(), NOT_FOUND_REPORT_COMMENT.getMessage());
+    }
 }
--- server/src/main/java/com/d201/goodshot/swing/exception/SwingExceptionList.java
+++ server/src/main/java/com/d201/goodshot/swing/exception/SwingExceptionList.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum SwingExceptionList {
 
     NOT_FOUND_SWING(HttpStatus.NOT_FOUND.value(), "존재하지 않는 스윙입니다."),
+    NOT_FOUND_REPORT_COMMENT(HttpStatus.NOT_FOUND.value(), "코멘트에 맞는 ENUM 값을 찾을 수 없습니다."),
     SWING_JSON_PROCESS_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "JSON 파싱을 처리하는 중에 문제가 발생했습니다.");
 
     private final int code;
