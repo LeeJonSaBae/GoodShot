d272975373260a982e89d1565ce49ef379171e2f
김민경 <org9899@naver.com>
2024-10-08 02:18:58 +0900

    ➕feat : 종합 리포트 피드백 도메인 생성

--- server/src/main/java/com/d201/goodshot/swing/domain/Report.java
+++ server/src/main/java/com/d201/goodshot/swing/domain/Report.java
@@ -1,5 +1,6 @@
 package com.d201.goodshot.swing.domain;
 
+import com.d201.goodshot.swing.enums.ProblemType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +20,9 @@ public class Report {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ProblemType problemType;
 
     private String content;
 
--- server/src/main/java/com/d201/goodshot/swing/repository/ReportRepository.java
+++ server/src/main/java/com/d201/goodshot/swing/repository/ReportRepository.java
@@ -1,2 +1,13 @@
-package com.d201.goodshot.swing.repository;public interface ReportRepository {
+package com.d201.goodshot.swing.repository;
+
+import com.d201.goodshot.swing.domain.Report;
+import com.d201.goodshot.swing.enums.ProblemType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Integer> {
+    List<Report> findByProblemType(ProblemType problemType);
 }
