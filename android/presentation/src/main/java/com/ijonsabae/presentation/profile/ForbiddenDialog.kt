ce6584457a66f7d1eb57851bda238f0d39dbcfd6
minseon <alstjs1516@gmail.com>
2024-10-08 14:35:49 +0900

    종합리포트 15회 이하인 경우 다이얼로그 띄우기 처리 완료

---
+++ android/presentation/src/main/java/com/ijonsabae/presentation/profile/ForbiddenDialog.kt
@@ -0,0 +1,31 @@
+package com.ijonsabae.presentation.profile
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import com.ijonsabae.presentation.R
+import com.ijonsabae.presentation.config.BaseDialog
+import com.ijonsabae.presentation.databinding.DialogForbiddenBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ForbiddenDialog :
+    BaseDialog<DialogForbiddenBinding>(DialogForbiddenBinding::bind, R.layout.dialog_forbidden) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setScreenWidthPercentage(0.9F)
+        setScreenHeightConstraint(WRAP_CONTENT)
+
+        binding.btnClose.setOnClickListener {
+            dismiss()
+        }
+
+        binding.btnYes.setOnClickListener {
+            dismiss()
+        }
+    }
+
+}
