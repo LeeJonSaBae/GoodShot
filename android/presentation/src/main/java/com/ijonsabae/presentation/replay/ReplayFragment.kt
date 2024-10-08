03f361c396ab73e8be338cbbeca25990a430ccf4
Hanguelmansae <111sym@naver.com>
2024-10-08 14:52:58 +0900

    ➕feat - 355 : Swing Report 내보내기, 가져오기 함수화

--- android/presentation/src/main/java/com/ijonsabae/presentation/replay/ReplayFragment.kt
+++ android/presentation/src/main/java/com/ijonsabae/presentation/replay/ReplayFragment.kt
@@ -1,28 +1,17 @@
 package com.ijonsabae.presentation.replay
 
 import android.app.Dialog
-import android.app.DownloadManager
-import android.content.Context
-import android.content.Context.DOWNLOAD_SERVICE
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
 import android.widget.ImageView
-import android.widget.Toast
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.net.toUri
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.ijonsabae.domain.model.SwingCommentExportImportParam
-import com.ijonsabae.domain.model.SwingComparisonParam
 import com.ijonsabae.domain.model.SwingFeedback
-import com.ijonsabae.domain.model.SwingFeedbackComment
-import com.ijonsabae.domain.model.SwingFeedbackExportImportParam
 import com.ijonsabae.domain.usecase.profile.UploadPresignedDataUseCase
 import com.ijonsabae.domain.usecase.replay.DeleteLocalSwingFeedbackUseCase
 import com.ijonsabae.domain.usecase.replay.ExportSwingFeedbackListUseCase
@@ -43,28 +32,18 @@ import com.ijonsabae.domain.usecase.shot.InsertLocalSwingFeedbackUseCase
 import com.ijonsabae.presentation.BuildConfig
 import com.ijonsabae.presentation.R
 import com.ijonsabae.presentation.config.BaseFragment
-import com.ijonsabae.presentation.config.Const.Companion.BACKSWING
-import com.ijonsabae.presentation.config.Const.Companion.BAD
-import com.ijonsabae.presentation.config.Const.Companion.DOWNSWING
-import com.ijonsabae.presentation.config.Const.Companion.NICE
 import com.ijonsabae.presentation.databinding.FragmentReplayBinding
 import com.ijonsabae.presentation.main.MainActivity
 import com.ijonsabae.presentation.mapper.SwingFeedbackCommentMapper
 import com.ijonsabae.presentation.mapper.SwingFeedbackMapper
-import com.ijonsabae.presentation.mapper.SwingFeedbackSyncRoomDataMapper
 import com.ijonsabae.presentation.shot.SwingLocalDataProcessor
-import com.ijonsabae.presentation.util.formatTDateFromLongKorea
-import com.ijonsabae.presentation.util.stringToTimeInMillis
+import com.ijonsabae.presentation.shot.SwingRemoteDataProcessor
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import java.io.File
-import java.io.FileOutputStream
 import javax.inject.Inject
 
 private const val TAG = "ReplayFragment_싸피"
@@ -83,33 +62,11 @@ class ReplayFragment :
     @Inject
     lateinit var updateLikeStatusUseCase: UpdateLikeStatusUseCase
     @Inject
-    lateinit var deleteLocalSwingFeedbackUseCase: DeleteLocalSwingFeedbackUseCase
-    @Inject
     lateinit var hideSwingFeedbackUseCase: HideSwingFeedbackUseCase
     @Inject
     lateinit var getLocalSwingFeedbackCommentUseCase: GetLocalSwingFeedbackCommentUseCase
     @Inject
-    lateinit var getChangedSwingFeedbackUseCase: GetLocalChangedSwingFeedbackListUseCase
-    @Inject
-    lateinit var getSwingFeedbackDataNeedSyncUseCase: GetLocalSwingFeedbackDataNeedSyncUseCase
-    @Inject
-    lateinit var getLocalSwingFeedbackListNeedToUploadUseCase: GetLocalSwingFeedbackListNeedToUploadUseCase
-    @Inject
-    lateinit var syncUpdateStatusUseCase: SyncUpdateStatusUseCase
-    @Inject
-    lateinit var getRemoteSwingFeedbackListNeedToUploadUseCase: GetRemoteSwingFeedbackListNeedToUploadUseCase
-    @Inject
-    lateinit var uploadPresignedDataUseCase: UploadPresignedDataUseCase
-    @Inject
-    lateinit var exportSwingFeedbackListUseCase: ExportSwingFeedbackListUseCase
-    @Inject
-    lateinit var importSwingFeedbackListUseCase: ImportSwingFeedbackListUseCase
-    @Inject
-    lateinit var getLocalSwingFeedbackListUseCase: GetLocalSwingFeedbackListUseCase
-    @Inject
-    lateinit var insertLocalSwingFeedbackUseCase: InsertLocalSwingFeedbackUseCase
-    @Inject
-    lateinit var insertLocalSwingFeedbackCommentUseCase: InsertLocalSwingFeedbackCommentUseCase
+    lateinit var swingRemoteDataProcessor: SwingRemoteDataProcessor
 
     private val viewModel: ReplayFragmentViewModel by viewModels()
 
@@ -220,81 +177,7 @@ class ReplayFragment :
 
         dialogView.findViewById<View>(R.id.menu_export).setOnClickListener {
             lifecycleScope.launch(coroutineExceptionHandler + Dispatchers.IO) {
-                val userID = viewModel.getUserID()
-                Log.d(TAG, "showCustomPopup: $userID")
-                if(userID == -1L){
-                    launch(Dispatchers.Main) {
-                        showToastShort("비회원은 동기화를 진행하실 수 없습니다!")
-                    }
-                }else{
-                    // 바꿔야 할 SwingFeedback 목록 추출
-                    val changedSwingFeedbackList = SwingFeedbackSyncRoomDataMapper.mapperToSwingFeedbackSyncList(getChangedSwingFeedbackUseCase(userID))
-                    // 서버에 업데이트 보냄
-                    getSwingFeedbackDataNeedSyncUseCase(changedSwingFeedbackList).getOrThrow()
-
-                    // 이후 업데이트 보냈으니까 삭제된 것 제외 전부 업데이트 0으로 해서 업데이트 필요 없는 값으로 설정
-                    syncUpdateStatusUseCase(userID)
-
-                    // 0인 목록들을 뽑아서 Upload 후보군을 생성
-                    val list = getLocalSwingFeedbackListNeedToUploadUseCase(userID)
-
-                    // 업로드 후보군으로부터 진짜 업로드 대상을 추출
-                    val result = getRemoteSwingFeedbackListNeedToUploadUseCase(SwingComparisonParam(list.map { it.swingCode})).getOrThrow()
-
-                    // 진짜 업로드 대상에 대해서 반복적으로 각 대상마다 10개의 영상, 이미지를 업로드
-                    result.data.forEach { data ->
-                        val poseList = SwingLocalDataProcessor.getSwingPoseFiles(fragmentContext, data.code, userID)
-                        data.presignedUrls.forEachIndexed { index, url ->
-                            when (index) {
-                                0 -> {
-                                    uploadPresignedDataUseCase(url, SwingLocalDataProcessor.getSwingVideoFile(fragmentContext,data.code, userID).toURI()).getOrThrow()
-                                }
-                                1 -> {
-                                    uploadPresignedDataUseCase(url, SwingLocalDataProcessor.getSwingThumbnailFile(fragmentContext,data.code, userID).toURI()).getOrThrow()
-                                }
-                                else -> {
-                                    uploadPresignedDataUseCase(url, poseList[index-2].toURI()).getOrThrow()
-                                }
-                            }
-                        }
-                    }
-
-                    // 업로드 대상에 대해서 돌아가면서 해당 업로드한 코드의 comment와 feedback을 가져와서 SwingExportParam 형성 및 Upload
-                    val codeSet = result.data.map { it.code }.toHashSet()
-
-                    exportSwingFeedbackListUseCase(
-                        list.filter {
-                            it.swingCode in codeSet
-                        }.map { it ->
-                            val data =
-                                SwingFeedbackExportImportParam(
-                                    score = it.score,
-                                    title = it.title,
-                                    similarity = it.similarity,
-                                    likeStatus = it.likeStatus,
-                                    tempo = it.tempo,
-                                    solution = it.solution,
-                                    id = userID,
-                                    code = it.swingCode,
-                                    time = formatTDateFromLongKorea(it.date),
-                                    backSwingComments = getLocalSwingFeedbackCommentUseCase(userID,it.swingCode).filter { comment ->
-                                        comment.poseType == BACKSWING
-                                    }.map { SwingCommentExportImportParam(
-                                        commentType = if(it.commentType == NICE){"NICE"} else{"BAD"},
-                                        content = it.content
-                                    ) },
-                                    downSwingComments = getLocalSwingFeedbackCommentUseCase(userID,it.swingCode).filter {
-                                        it.poseType == DOWNSWING
-                                    }.map { SwingCommentExportImportParam(
-                                        commentType = if(it.commentType == NICE){"NICE"} else{"BAD"},
-                                        content = it.content
-                                    ) }
-                                )
-                            Log.d(TAG, "showCustomPopup 업로드 데이터: $data")
-                            data
-                        }
-                    ).getOrThrow()
-                }
+                swingRemoteDataProcessor.uploadLocalSwingData(fragmentContext)
                 withContext(Dispatchers.Main) {
                     showToastShort("내보내기가 완료됐습니다!")
                 }
@@ -304,86 +187,7 @@ class ReplayFragment :
         }
         dialogView.findViewById<View>(R.id.menu_import).setOnClickListener {
             lifecycleScope.launch(coroutineExceptionHandler + Dispatchers.IO) {
-                val userID = viewModel.getUserID()
-                val list = getLocalSwingFeedbackListUseCase(userID)
-                val swingComparisonParamList = SwingComparisonParam(list.map { it.swingCode})
-                val result = importSwingFeedbackListUseCase(swingComparisonParamList).getOrThrow()
-                result.data.forEach { swingFeedbackParam ->
-                    insertLocalSwingFeedbackUseCase(
-                        SwingFeedback(
-                            userID = userID,
-                            title = swingFeedbackParam.title,
-                            swingCode = swingFeedbackParam.code,
-                            tempo = swingFeedbackParam.tempo,
-                            date = stringToTimeInMillis(swingFeedbackParam.time),
-                            score = swingFeedbackParam.score,
-                            likeStatus = swingFeedbackParam.likeStatus,
-                            similarity = swingFeedbackParam.similarity,
-                            solution = swingFeedbackParam.solution
-                        )
-                    )
-                    swingFeedbackParam.downSwingComments.forEach { comment ->
-                        insertLocalSwingFeedbackCommentUseCase(
-                            SwingFeedbackComment(
-                                userID = userID,
-                                swingCode = swingFeedbackParam.code,
-                                poseType = DOWNSWING,
-                                content = comment.content,
-                                commentType = if(comment.commentType == "NICE"){NICE}else{BAD}
-                            )
-                        )
-                    }
-                    swingFeedbackParam.backSwingComments.forEach { comment ->
-                        insertLocalSwingFeedbackCommentUseCase(
-                            SwingFeedbackComment(
-                                userID = userID,
-                                swingCode = swingFeedbackParam.code,
-                                poseType = BACKSWING,
-                                content = comment.content,
-                                commentType = if(comment.commentType == "NICE"){NICE}else{BAD}
-                            )
-                        )
-                    }
-                    // DownloadManager에 요청 추가
-                    val downloadManager = fragmentContext.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-                    val videoUri = Uri.parse(S3_URL + userID + VIDEO + swingFeedbackParam.code + ".mp4") // [파일 다운로드 주소 : 확장자명 포함되어야함]
-                    val thumbnailUri = Uri.parse(S3_URL + userID + THUMBNAIL + swingFeedbackParam.code + ".jpg") // [파일 다운로드 주소 : 확장자명 포함되어야함]
-                    Log.d(TAG, "showCustomPopup: $videoUri")
-                    fun returnImageUri(idx: Int): Uri{
-                        return Uri.parse(S3_URL + userID + IMAGE + swingFeedbackParam.code + "_" + idx + ".jpg") // [파일 다운로드 주소 : 확장자명 포함되어야함]
-                    }
-
-
-                    Log.d(TAG, "showCustomPopup: ${SwingLocalDataProcessor.getSwingVideoFile(fragmentContext, userId = userID, swingCode = swingFeedbackParam.code).path}")
-                    downloadAndSaveFile(videoUri.toString(),  SwingLocalDataProcessor.getSwingVideoFile(fragmentContext, userId = userID, swingCode = swingFeedbackParam.code).toString())
-                    downloadAndSaveFile(thumbnailUri.toString(), SwingLocalDataProcessor.getSwingThumbnailFile(fragmentContext, userId = userID, swingCode = swingFeedbackParam.code).toString())
-                    val poseDestination = SwingLocalDataProcessor.getSwingPoseFiles(fragmentContext, userId = userID, swingCode = swingFeedbackParam.code)
-                    for(i in 0 until 8){
-                        downloadManager.apply {
-                            val imageUrl = returnImageUri(i)
-                            downloadAndSaveFile(imageUrl.toString(), poseDestination[i].toPath().toString())
-//                            enqueue(DownloadManager.Request(imageUrl).setDestinationUri(poseDestination[i].toUri()))
-                        }
-                    }
-
-
-//                    downloadManager.apply {
-//                        val swingDir = File(fragmentContext.cacheDir, "video/$userID")
-//                        if (!swingDir.exists()) {
-//                            swingDir.mkdirs()
-//                        }
-//                        enqueue(DownloadManager.Request(videoUri).setDestinationUri(fragmentContext.cacheDir.toUri()))
-////                        enqueue(DownloadManager.Request(videoUri).setDestinationUri(SwingLocalDataProcessor.getSwingVideoFile(fragmentContext, userId = userID, swingCode = swingFeedbackParam.code).toUri()))
-//                        enqueue(DownloadManager.Request(thumbnailUri).setDestinationUri(SwingLocalDataProcessor.getSwingThumbnailFile(fragmentContext, userId = userID, swingCode = swingFeedbackParam.code).toUri()))
-//                    }
-//                    val poseDestination = SwingLocalDataProcessor.getSwingPoseFiles(fragmentContext, userId = userID, swingCode = swingFeedbackParam.code)
-//                    for(i in 0 .. 8){
-//                        downloadManager.apply {
-//                            val imageUrl = returnImageUri(i)
-//                            enqueue(DownloadManager.Request(imageUrl).setDestinationUri(poseDestination[i].toUri()))
-//                        }
-//                    }
-                }
+               swingRemoteDataProcessor.downloadRemoteSwingData(fragmentContext)
                 if(binding.cbFilter.isChecked){
                     viewModel.getLocalSwingFeedbackLikeList()
                     lifecycleScope.launch {
@@ -446,26 +250,6 @@ class ReplayFragment :
             setOnTouchListener { v, event ->
                 swipeHelper.removePreviousClamp(this)
                 false
-            }
-        }
-    }
-
-    suspend fun downloadAndSaveFile(presignedUrl: String, saveFilePath: String) {
-        withContext(Dispatchers.IO) {
-            val client = OkHttpClient()
-            val request = Request.Builder().url(presignedUrl).build()
-
-            client.newCall(request).execute().use { response ->
-                if (!response.isSuccessful) throw Exception("Failed to download file: ${response.code} ${response.message}")
-
-                val body = response.body ?: throw Exception("Empty response body")
-                val file = File(saveFilePath)
-
-                FileOutputStream(file).use { outputStream ->
-                    body.byteStream().use { inputStream ->
-                        inputStream.copyTo(outputStream)
-                    }
-                }
             }
         }
     }
